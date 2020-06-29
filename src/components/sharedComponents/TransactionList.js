e701405dba6005eb019feb50b9a452a1f95cdc52
Ella Aronov <ellaarono1994@gmail.com>
2020-06-29 20:30:21 +0300

    Switched from the usage of ul to table in order to present the transaction history

--- src/components/sharedComponents/TransactionList.js
+++ src/components/sharedComponents/TransactionList.js
@@ -5,7 +5,16 @@ const TransactionList = ({ transactionActionType, transactionList }) => {
     <React.Fragment>
       <h2 className="header-history">{`${transactionActionType} History`}</h2>
       <div className="transactions">
-        <ul className="transaction-list">{transactionList}</ul>
+        <table>
+          <thead>
+            <tr>
+              <th scope="col">transaction</th>
+              <th scope="col">Amount</th>
+              <th scope="col">Delete</th>
+            </tr>
+          </thead>
+          <tbody>{transactionList}</tbody>
+        </table>
       </div>
     </React.Fragment>
   );
--- src/components/sharedComponents/TransactionListItem.js
+++ src/components/sharedComponents/TransactionListItem.js
@@ -5,15 +5,18 @@ const TransactionListItem = ({ id, type, amount, deleteFunction }) => {
   const dispatch = useDispatch();
 
   return (
-    <li className="transaction">
-      <span>{`${type}, the amount is: $${amount}`}</span>
-      <button
-        className="delete-btn"
-        onClick={() => dispatch(deleteFunction(id))}
-      >
-        <i className="fas fa-trash"></i>
-      </button>
-    </li>
+    <tr>
+      <td>{type}</td>
+      <td>{`$${amount}`}</td>
+      <td>
+        <button
+          className="delete-btn"
+          onClick={() => dispatch(deleteFunction(id))}
+        >
+          <i className="fas fa-trash"></i>
+        </button>
+      </td>
+    </tr>
   );
 };
 
